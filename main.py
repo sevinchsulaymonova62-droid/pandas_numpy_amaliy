97521f49731bad128463b89541e186dbbf1a84a1
sevinchsulaymonova62-droid <sevinchsulaymonova62@gmail.com>
2026-01-08 23:59:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -49,5 +49,34 @@ cars = {
 
 df_cars = pd.DataFrame(cars)
 print(df_cars)
+import zipfile
+import os
+
+
+4-amaliy mashg'ulot
+# 1-TOPSHIRIQ: Ma'lumotlarni siqish (Ziplash)
+filename = "malumot.txt"
+with open(filename, "w") as f:
+    f.write("Bu amaliy ish uchun yaratilgan test ma'lumoti. " * 100)
+
+zip_name = "arxiv.zip"
+with zipfile.ZipFile(zip_name, 'w') as zipf:
+    zipf.write(filename, compress_type=zipfile.ZIP_DEFLATED)
+
+print(f"1. {filename} fayli {zip_name} ichiga siqildi.")
+
+# 2-TOPSHIRIQ: Siqilgan faylni ochish va tekshirish
+with zipfile.ZipFile(zip_name, 'r') as zipf:
+    zipf.extractall("ochilgan_fayllar")
+    content = zipf.namelist()
+
+# Xulosa
+original_size = os.path.getsize(filename)
+zip_size = os.path.getsize(zip_name)
+
+print("\n--- XULOSA ---")
+print(f"Asl fayl hajmi: {original_size} bayt")
+print(f"Siqilgan fayl hajmi: {zip_size} bayt")
+print(f"Natija: Fayl hajmi {((original_size - zip_size) / original_size) * 100:.2f}% ga kamaydi.")
 
 
